1dd6f7633450c4cf9440fab3b1f89d89aee6e672
Shubam Bhasin <33823641+shubambhasin@users.noreply.github.com>
2021-02-15 15:51:35 +0530

    fix: check form fix

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <div className="contact">
       <div className="contact-container">
-        <form  method="POST" data-netlify="true" data-netlify-recaptcha="true">
+        <form netlify data-netlify-recaptcha="true">
           <h2 style={{ textAlign: "left" }}>Get in Touch</h2>
 
           <input placeholder="Enter your name..." type="text" />
