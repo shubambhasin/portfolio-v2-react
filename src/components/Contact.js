415f3d9093b715a7cb99094170e1b9f4ecd4562a
Shubam Bhasin <33823641+shubambhasin@users.noreply.github.com>
2021-02-26 15:12:59 +0530

    check again

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -14,12 +14,12 @@ useEffect(() => {
         <form name= "contact" action="https://formspree.io/f/xnqoknyl" method="POST"  netlify >
           <h2 style={{ textAlign: "left" }}>Get in Touch</h2>
 
-          <input ref={inputRef} placeholder="Enter your name..." type="text" />
+          <input ref={inputRef} name="name" placeholder="Enter your name..." type="text" />
           <br />
 
-          <input placeholder="Enter your email..." type="email" />
+          <input name="email" placeholder="Enter your email..." type="email" />
           <br />
-          <textarea placeholder="Enter your message" />
+          <textarea name="Message" placeholder="Enter your message" />
           <br />
           <div data-netlify-recaptcha="true"></div>
           <button type="submit">Send</button>
